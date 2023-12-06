af33c73efe149f1d9cbf44a3fe1967adf877c465
jhwrwe <jalexander05@student.ciputra.ac.id>
2023-12-06 14:52:06 +0700

    ini

--- app/src/main/java/com/example/alpvisualprogramming/ui/theme/Color.kt
+++ app/src/main/java/com/example/alpvisualprogramming/ui/theme/Color.kt
@@ -5,7 +5,10 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
+val BlueOrigin = Color(0xFF3960E)
+val BlueOriginLDarker = Color(0xFF3657C9)
+val BlueOriginDarker = Color(0xFF2845A7)
+val fontgray = Color(0xFF3F3F3F)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
--- app/src/main/java/com/example/alpvisualprogramming/ui/views/SignInPage.kt
+++ app/src/main/java/com/example/alpvisualprogramming/ui/views/SignInPage.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -31,40 +36,39 @@ import androidx.compose.ui.unit.sp
 fun SignInPageView(){
     Column (modifier = Modifier
         .fillMaxSize()
-        .background(Color.Blue),
-        verticalArrangement = Arrangement.Center){
-        Column (verticalArrangement = Arrangement.Center){
+        .background(Color(0xFF3960E5)),
+        verticalArrangement = Arrangement.SpaceBetween){
+        Column (verticalArrangement = Arrangement.Center, modifier = Modifier.padding(top=200.dp)){
             Text(text = "Welcome to,",color = Color.White, fontSize = 40.sp, fontWeight = FontWeight.Bold)
             Text(text = "Application name", color = Color.White,fontSize = 40.sp, fontWeight = FontWeight.Bold)
         }
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color.White)
-                .border(
-                    1.dp,
-                    Color.White
-                )
-                .clip(RoundedCornerShape(topEnd = 100.dp))
+                .clip(RoundedCornerShape(0.dp, 64.dp, 0.dp, 0.dp))
+                .border(2.dp, Color.Gray, RoundedCornerShape(0.dp, 64.dp, 0.dp, 0.dp))
         ) {
-            Text(text = "Be more productive with '...' and start", color = Color.Gray, fontWeight = FontWeight.SemiBold,fontSize = 15.sp, modifier = Modifier
+            Text(text = "Be more productive with '...' and start", color = Color(0xFF3F3F3F), fontWeight = FontWeight.SemiBold,fontSize = 15.sp, modifier = Modifier
                 .padding(horizontal = 20.dp)
                 .padding(vertical = 2.dp))
-            Text(text = "writing your to-do list", color = Color.Gray, fontWeight = FontWeight.SemiBold,fontSize = 15.sp, modifier = Modifier
+            Text(text = "writing your to-do list", color = Color(0xFF3F3F3F), fontWeight = FontWeight.SemiBold,fontSize = 15.sp, modifier = Modifier
                 .padding(horizontal = 20.dp)
                 .padding(vertical = 2.dp))
             Button(onClick = { /*TODO*/ }, modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp),colors = ButtonDefaults.buttonColors(Color.Blue)) {
+                .padding(10.dp),colors = ButtonDefaults.buttonColors(Color(0xFF3960E5))) {
                 Text(text = "Sign In", fontWeight = FontWeight.Bold,fontSize = 15.sp)
             }
 4
 
-            Column (modifier = Modifier.fillMaxWidth()){
-                Text(text = "Create Account",color = Color.Gray, textAlign = TextAlign.Right,modifier = Modifier.fillMaxWidth())
+            Row (modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp)){
+                Text(text = "Create Account",color = Color(0xFF3F3F3F), fontWeight = FontWeight.Bold, textAlign = TextAlign.Right,modifier = Modifier.fillMaxWidth())
+                Icon(Icons.Rounded.ArrowForward, contentDescription = "Back")
             }
-
-
         }
     }
 }
