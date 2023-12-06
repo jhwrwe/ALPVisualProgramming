ea0227d6cf45f88232072daf21aeb4f4c405052a
jhwrwe <jalexander05@student.ciputra.ac.id>
2023-12-06 13:28:55 +0700

    First View
    This is the sign in view for main page

---
+++ app/src/main/java/com/example/alpvisualprogramming/ui/views/SignInPage.kt
@@ -0,0 +1,61 @@
+package com.example.alpvisualprogramming.ui.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SignInPageView(){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Blue),
+        verticalArrangement = Arrangement.Center){
+        Column (verticalArrangement = Arrangement.Center){
+            Text(text = "Welcome to,",color = Color.White, fontSize = 40.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Application name", color = Color.White,fontSize = 40.sp, fontWeight = FontWeight.Bold)
+        }
+       Divider()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .border(
+                    1.dp,
+                    Color.White
+                )
+                .clip(RoundedCornerShape(topEnd = 100.dp))
+        ) {
+            Text(text = "Be more productive with '...' and start", color = Color.Gray, fontWeight = FontWeight.SemiBold,fontSize = 15.sp)
+            Text(text = "writing your to-do list", color = Color.Gray, fontWeight = FontWeight.SemiBold,fontSize = 15.sp)
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+                Text(text = "Sign In")
+                
+            }
+        }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SignInPagePreview(){
+    SignInPageView()
+}
