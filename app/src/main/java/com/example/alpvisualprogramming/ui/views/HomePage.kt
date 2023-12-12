3a1e0f86bdc55e8a58a9fcc255cea4b499536f81
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2023-12-12 13:17:33 +0700

    Homepage dan Todolist

---
+++ app/src/main/java/com/example/alpvisualprogramming/ui/views/HomePage.kt
@@ -0,0 +1,272 @@
+package com.example.alpvisualprogramming.ui.views
+
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.alpvisualprogramming.R
+
+@Composable
+fun MainPageView(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column (
+            modifier = Modifier.background(Color(0xFF3960E6)),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                modifier = Modifier.padding(top = 10.dp),
+                text = "Application Name",
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = 24.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                ) {
+                    Text(
+                        text = "Hello,",
+                        fontSize = 28.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Richie Reuben",
+                        fontSize = 28.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_account_circle_24),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(CircleShape)
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "Priority Matrix",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color(0xFF3F3F3F),
+                modifier = Modifier.padding(vertical = 12.dp)
+            )
+            // Card dengan tulisan "Schedule" di atas dan "Points" di bawah
+            LazyRow(
+                content = {
+                    item {
+                        ColoredCard("Schedule", "Points", Color(0xFF00CD00))
+                    }
+                    item {
+                        ColoredCard("Meetings", "Agenda", Color(0xFF007AFF))
+                    }
+                    item {
+                        ColoredCard("Tasks", "Progress", Color(0xFFDB8901))
+                    }
+                    item {
+                        ColoredCard("Events", "Details", Color(0xFFDF013A))
+                    }
+                }
+            )
+            Text(
+                text = "Deadline",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color(0xFF3F3F3F),
+                modifier = Modifier.padding(bottom = 12.dp, top = 20.dp)
+            )
+            IconTextNumberCard(
+                imageResId = R.drawable.icons8_time_96,
+                text = "Too Late",
+                number = 42
+            )
+            IconTextNumberCard(
+                imageResId = R.drawable.icons8_deadline_96,
+                text = "Today",
+                number = 42
+            )
+            IconTextNumberCard(
+                imageResId = R.drawable.icons8_do_not_disturb_ios_96,
+                text = "Tomorrow",
+                number = 42
+            )
+            IconTextNumberCard(
+                imageResId = R.drawable.icons8_question_96,
+                text = "Someday",
+                number = 42
+            )
+            IconTextNumberCard(
+                imageResId = R.drawable.icons8_no_data_availible_96,
+                text = "No Deadline",
+                number = 42
+            )
+            IconTextNumberCard(
+                imageResId = R.drawable.icons8_done_96,
+                text = "Done",
+                number = 42
+            )
+        }
+    }
+}
+
+@Composable
+fun ColoredCard(title: String, subtitle: String, color: Color) {
+    val gradient = Brush.horizontalGradient(
+        colors = listOf(
+            color,
+            color.copy(alpha = 0.8f),
+            color.copy(alpha = 0.5f)
+        )
+    )
+
+    Card(
+        modifier = Modifier
+            .width(150.dp)
+            .height(100.dp)
+            .padding(end = 8.dp),
+        shape = MaterialTheme.shapes.medium,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .drawWithContent {
+                    drawRect(brush = gradient)
+                    drawContent()
+                },
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 12.dp, horizontal = 16.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = subtitle,
+                    color = Color.White,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun IconTextNumberCard(imageResId: Int, text: String, number: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .padding(bottom = 10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFE4EFFF)
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 4.dp, horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Ikon di sebelah kiri
+            Image(
+                painter = painterResource(id = imageResId),
+                contentDescription = null,
+                modifier = Modifier.size(30.dp)
+            )
+
+            // Teks di sebelah ikon
+            Text(
+                text = text,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                color = Color(0xFF3F3F3F),
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .weight(1f)
+            )
+
+            // Angka di pojok kanan
+            Text(
+                text = number.toString(),
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                color = Color(0xFF3F3F3F),
+            )
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainPagePreview(){
+    MainPageView()
+}
---
+++ app/src/main/java/com/example/alpvisualprogramming/ui/views/TodoList.kt
@@ -0,0 +1,230 @@
+package com.example.alpvisualprogramming.ui.views
+
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.alpvisualprogramming.R
+
+@Composable
+fun TodoListView(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .background(Color(0xFF3960E6))
+                .fillMaxWidth()
+                .padding(vertical = 10.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(30.dp)
+            )
+            Spacer(modifier = Modifier.width(115.dp))
+            Text(
+                text = "Task To Do",
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 12.dp)
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.frame_5),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(35.dp)
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(
+                text = "Do First",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color(0xFF3F3F3F),
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(
+                text = "8 Task",
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                color = Color(0xFF8B8E91),
+                modifier = Modifier.padding(top = 10.dp)
+            )
+        }
+        Text(
+            text = "Urgent and important.  Give this task the highest priority.",
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+            color = Color(0xFF3F3F3F),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        CardWithCheckbox()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_add_circle_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.BottomEnd)
+                    .padding(8.dp) // Adjust padding as needed
+            )
+        }
+    }
+}
+
+@Composable
+fun CardWithCheckbox() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFE4EFFF)
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(
+                checked = false, // Set the initial checked state
+                onCheckedChange = { /* Handle checkbox state change */ },
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                modifier = Modifier
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    text = "OS Presentation",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column {
+                        Text(
+                            text = "At Ciputra University",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 12.sp,
+                            color = Color(0xFF8B8E91)
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "Friday, 7 May 2023 | 15.00 WIB",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 12.sp,
+                            color = Color(0xFF8B8E91)
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .width(50.dp)
+                            .padding(start = 12.dp),
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.baseline_access_time_filled_24),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(25.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.baseline_account_circle_24),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(25.dp)
+                                .offset(10.dp)
+                        )
+                    }
+                    Text(
+                        text = "+ 2",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp,
+                        color = Color(0xFF8B8E91)
+                    )
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.icons8_delete_96),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(20.dp)
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TodoListPreview(){
+    TodoListView()
+}
