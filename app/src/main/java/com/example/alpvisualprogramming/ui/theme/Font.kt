f62f473988e3d19c22d73c8488c8479bdfe5797c
xAnonym101 <xvgonst@gmail.com>
2023-12-13 14:53:43 +0700

    Adding Font-Style

---
+++ app/src/main/java/com/example/alpvisualprogramming/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.example.alpvisualprogramming.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.alpvisualprogramming.R
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_medium, FontWeight.Medium)
+)
