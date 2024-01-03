30428153f0e50a54b5b151ec6294b6e1f27ca892
xAnonym101 <xvgonst@gmail.com>
2024-01-03 16:45:50 +0700

    minor BadgeVM Upd

--- app/src/main/java/com/example/alpvisualprogramming/ui/viewmodel/BadgeVM.kt
+++ app/src/main/java/com/example/alpvisualprogramming/ui/viewmodel/BadgeVM.kt
@@ -2,46 +2,63 @@ package com.example.alpvisualprogramming.ui.viewmodel
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import com.example.alpvisualprogramming.model.Badge
 import com.example.alpvisualprogramming.model.BadgeUser
-import com.example.alpvisualprogramming.model.Mission
-import com.example.alpvisualprogramming.model.User
 import com.example.alpvisualprogramming.repositories.MyDBContainer
-import com.example.alpvisualprogramming.ui.NavGraph
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class BadgeVM: ViewModel() {
+class BadgeVM : ViewModel() {
 
-    suspend fun GetAllUserBadge(): List<Badge>? {
-        val finnish = MyDBContainer().myDBRepositories.getAllUserBadge()
-        return finnish
+    private val _badges = MutableStateFlow<List<Badge>>(emptyList())
+    val badges: StateFlow<List<Badge>> = _badges.asStateFlow()
+
+    private val _uBadges = MutableStateFlow<List<Badge>>(emptyList())
+    val uBadges: StateFlow<List<Badge>> = _uBadges.asStateFlow()
+
+    init {
+        GetAllUserBadge()
+        GetAllBadge()
     }
 
-    suspend fun GetAllBadge(): List<Badge>? {
-        val finnish = MyDBContainer().myDBRepositories.getAllBadge()
-        return finnish
+    fun GetAllUserBadge(): List<Badge>? {
+        var uBadges: List<Badge> = emptyList()
+        viewModelScope.launch {
+            uBadges = MyDBContainer().myDBRepositories.getAllUserBadge()
+            _uBadges.value = uBadges
+        }
+        return uBadges
     }
 
-    fun CreateBadgeUser(user_id:Int, badge_id:Int, context:Context){
+    fun GetAllBadge(): List<Badge>? {
+        var badges: List<Badge> = emptyList()
+        viewModelScope.launch {
+            badges = MyDBContainer().myDBRepositories.getAllBadge()
+            _badges.value = badges
+        }
+        return badges
+    }
+
+    fun CreateBadgeUser(user_id: Int, badge_id: Int, context: Context) {
         viewModelScope.launch {
             val badgeUser = BadgeUser(badge_id, user_id)
             val finnish = MyDBContainer().myDBRepositories.user_badge(badgeUser, badge_id)
 //            if (finnish.equals("Success",true)){
-                Toast.makeText(context,finnish,Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, finnish, Toast.LENGTH_SHORT).show()
 //            }
         }
     }
 
-    fun DecreaseCoins(user_id:Int, badge_id:Int, context:Context){
+    fun DecreaseCoins(user_id: Int, badge_id: Int, context: Context) {
         viewModelScope.launch {
             val badgeUser = BadgeUser(badge_id, user_id)
             val finnish = MyDBContainer().myDBRepositories.coins_minus(badgeUser, badge_id)
 //            if (finnish.equals("Success",true)){
-            Toast.makeText(context,finnish,Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, finnish, Toast.LENGTH_SHORT).show()
 //            }
         }
     }
