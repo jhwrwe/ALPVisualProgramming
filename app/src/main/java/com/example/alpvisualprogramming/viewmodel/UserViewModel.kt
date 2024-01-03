badc6c918fe596a3ac87880488c9392309c7c83d
jhwrwe <jalexander05@student.ciputra.ac.id>
2024-01-03 13:27:33 +0700

    user view model untuk register dan login

--- app/src/main/java/com/example/alpvisualprogramming/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/alpvisualprogramming/viewmodel/UserViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.example.alpvisualprogramming.Data.DataStoreManager
+import com.example.alpvisualprogramming.model.User
 import com.example.alpvisualprogramming.repositories.MyDBContainer
 import kotlinx.coroutines.launch
 
@@ -18,7 +19,7 @@ class UserViewModel : ViewModel(){
            }else if(token.equals("User not found",true)){
                Toast.makeText(context, token, Toast.LENGTH_LONG).show()
            }else {
-               navController.navigate(ListScreen.ListMovie.name){
+               navController.navigate(ListScreen.ListApp.name){
                    popUpTo(ListScreen.Login.name){inclusive=true}
                }
                dataStore.saveToken(token)
@@ -30,5 +31,17 @@ class UserViewModel : ViewModel(){
                }
            }
        }
+    }
+
+    fun ButtonRegister(username:String, fullname:String , phone_number:String,email:String, profile_photo_path:String, password: String, context: Context, navController: NavController){
+        viewModelScope.launch {
+            val CreateUsernotpermanent = User(fullname, phone_number, username, 0, 2, email, profile_photo_path, password)
+            val finnish = MyDBContainer().myDBRepositories.register(CreateUsernotpermanent)
+            if (finnish.equals("Success",true)){
+                navController.navigate(ListScreen.Login.name){
+                    popUpTo(ListScreen.Register.name)
+                }
+            }
+        }
     }
 }
