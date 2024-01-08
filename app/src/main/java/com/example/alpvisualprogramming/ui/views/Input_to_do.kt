0d52d8c26173b904c70aaa08e3bf6d5d4b37762e
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-01-09 04:38:59 +0700

    update semua done

--- app/src/main/java/com/example/alpvisualprogramming/ui/views/Input_to_do.kt
+++ app/src/main/java/com/example/alpvisualprogramming/ui/views/Input_to_do.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -60,6 +61,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.alpvisualprogramming.globalvariable.GlobalVariable
 import com.example.alpvisualprogramming.ui.viewmodel.TodolistVM
 import com.maxkeppeker.sheets.core.models.base.rememberSheetState
 import com.maxkeppeler.sheets.calendar.CalendarDialog
@@ -70,6 +72,7 @@ import com.maxkeppeler.sheets.clock.ClockDialog
 import com.maxkeppeler.sheets.clock.models.ClockSelection
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
@@ -309,6 +312,269 @@ fun InputToDo(navController: NavController,
             Button(
                 onClick = {
                     todolistViewModel.ButtonSubmitTodolist(title,dates,time,selectedItem,description,location, navController )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 50.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF3960E5)),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text(text = "Create", fontWeight = FontWeight.Bold, fontSize = 15.sp)
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateToDo(navController: NavController,
+              todolistViewModel: TodolistVM) {
+    var expanded by remember{ mutableStateOf(false) }
+    var list = listOf("Do First", "Schedule", "Delegate", "Eliminate")
+    var selectedItem by remember { mutableStateOf("") }
+    var textfiledsize by remember { mutableStateOf(Size.Zero) }
+    val icon = if (expanded){
+        Icons.Filled.KeyboardArrowUp
+    }else{
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    val todolistsDetail by GlobalVariable.todolistDetail.collectAsState()
+
+    var id by rememberSaveable { mutableStateOf(0) }
+    var title by rememberSaveable { mutableStateOf("") }
+    var dates by rememberSaveable { mutableStateOf("") }
+    var time by rememberSaveable { mutableStateOf("") }
+    var location by rememberSaveable { mutableStateOf("") }
+    var description by rememberSaveable { mutableStateOf("") }
+    var category by rememberSaveable { mutableStateOf("") }
+    var timesicon by remember{ mutableStateOf(false) }
+    var spaceicon by remember{ mutableStateOf(false) }
+    id = todolistsDetail?.id!!.toInt()
+    title = todolistsDetail?.title.toString()
+    dates = todolistsDetail?.date.toString()
+    time = todolistsDetail?.time.toString()
+    location = todolistsDetail?.location.toString()
+    description = todolistsDetail?.description.toString()
+    selectedItem = todolistsDetail?.urgency_status.toString()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        val calenderstate = rememberSheetState()
+        CalendarDialog(
+            state = calenderstate,
+            config = CalendarConfig(
+                monthSelection = true,
+                yearSelection = true,
+                style = CalendarStyle.MONTH,
+                disabledDates = listOf(LocalDate.now().plusDays(7))
+            ),
+            selection = CalendarSelection.Date{date ->
+                Log.d("selectedDate","$date")
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                dates = formatter.format(date)
+            }
+        )
+        val clockstate = rememberSheetState()
+        ClockDialog(state = clockstate, selection = ClockSelection.HoursMinutes{hours, minutes ->
+            Log.d("selectedDate","$hours:$minutes")
+            time = "$hours:$minutes"
+        })
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF3960E5))
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.ArrowBack,
+                contentDescription = "Arrow Icon",
+                tint = Color.White
+            )
+            Text(
+                text = "Add new task",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(1F)
+            )
+        }
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text(
+                text = "Title",
+                fontSize = 15.sp,
+                color = Color.Gray,
+                fontWeight = FontWeight.SemiBold
+            )
+            customTextFielda(
+                value = title,
+                onValueChanged = { title = it },
+                text = "Add Task Title",
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Column (){
+                Text(
+                    text = "Category",
+                    fontSize = 15.sp,
+                    color = Color.Gray,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(top = 10.dp)
+                )
+//            CustomTextField(
+//                value = category,
+//                onValueChanged = { category = it },
+//                text = "Do first",
+//                keyboardOptions = KeyboardOptions.Default.copy(
+//                    keyboardType = KeyboardType.Text,
+//                    imeAction = ImeAction.Next
+//                ),
+//                modifier = Modifier.fillMaxWidth()
+//            )
+                OutlinedTextField(
+                    value = selectedItem,
+                    onValueChange = {selectedItem=it},
+                    modifier= Modifier
+                        .fillMaxWidth()
+                        .onGloballyPositioned { coordinates ->
+                            textfiledsize = coordinates.size.toSize()
+                        },
+                    label = { Text(text = "Selected Item")},
+                    trailingIcon = {
+                        Icon(icon,"",Modifier.clickable { expanded = !expanded })
+                    }
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded= false },
+                    modifier = Modifier
+                        .width(with(LocalDensity.current){textfiledsize.width.toDp()})) {
+                    list.forEach {label->
+                        DropdownMenuItem(text = { Text(text = label) }, onClick = {
+                            selectedItem = label
+                            expanded = false } ,Modifier.background(Color.White))
+
+                    }
+                }
+            }
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(Modifier.weight(0.95F)) {
+                    Text(
+                        text = "Date",
+                        fontSize = 15.sp,
+                        color = Color.Gray,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    OutlinedIconButton(
+                        onClick = {
+                            spaceicon = !spaceicon
+                            calenderstate.show()
+                        },modifier=Modifier.fillMaxWidth()) {
+                        if(!spaceicon){
+                            Icon(imageVector = Icons.Outlined.DateRange, contentDescription = "Date Picker Icon")
+                        }
+                        Text(text = dates)
+                    }
+
+//                    DatePicker(
+//                        date = dates,
+//                        onDateSelected = { selectedDate -> dates = selectedDate },
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Column(Modifier.weight(0.95F)) {
+                    Text(
+                        text = "Time",
+                        fontSize = 15.sp,
+                        color = Color.Gray,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    OutlinedIconButton(onClick = {
+                        timesicon =!timesicon
+                        clockstate.show()
+                    },modifier=Modifier.fillMaxWidth()) {
+                        if(!timesicon){
+                            Icon(imageVector = Icons.Outlined.Schedule, contentDescription = "Date Picker Icon")
+                        }
+                        Text(text = time)
+                    }
+//                    TimePicker(
+//                        time = time,
+//                        onTimeSelected = { selectedTime -> time = selectedTime },
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+                }
+            }
+            Text(
+                text = "Location",
+                fontSize = 15.sp,
+                color = Color.Gray,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(top = 10.dp)
+            )
+            customTextFielda(
+                value = location,
+                onValueChanged = { location = it },
+                text = "Juanda airport",
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = "Description",
+                fontSize = 15.sp,
+                color = Color.Gray,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(top = 10.dp)
+            )
+            customTextFielda(
+                value = description,
+                onValueChanged = { description = it },
+                text = "badadad",
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Row(
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(Modifier.weight(0.95F)) {
+                    Button(
+                        onClick = {  },
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(Color(0xFF3960E5)),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Text(text = "Invite people + ", fontWeight = FontWeight.Bold, fontSize = 15.sp)
+                    }
+                }
+                Column(Modifier.weight(0.95F)) {
+
+                }
+            }
+            Button(
+                onClick = {
+                    todolistViewModel.updateTodolist(id, title,dates,time,selectedItem,description,location, navController )
                 },
                 modifier = Modifier
                     .fillMaxWidth()
